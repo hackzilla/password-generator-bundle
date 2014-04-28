21e3c1a1d994c07d52523b912ac0ba4846833d87
Daniel Platt <github@ofdan.co.uk>
2014-04-28 22:09:58 +0100

    remove unused variables

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -19,7 +19,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('hackzilla_password_generator');
+        $treeBuilder->root('hackzilla_password_generator');
 
         return $treeBuilder;
     }
