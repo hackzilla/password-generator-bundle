643f63670bc14c2a821dc9823989d24a30f106ec
Daniel Platt <github@ofdan.co.uk>
2016-07-25 08:10:44 +0100

    remove . from end of param and return statements

--- Tests/Controller/GeneratorControllerTest.php
+++ Tests/Controller/GeneratorControllerTest.php
@@ -50,11 +50,11 @@ class GeneratorControllerTest extends \PHPUnit_Framework_TestCase
     /**
      * Call protected/private method of a class.
      *
-     * @param object &$object    Instantiated object that we will run method on.
+     * @param object &$object    Instantiated object that we will run method on
      * @param string $methodName Method name to call
-     * @param array  $parameters Array of parameters to pass into method.
+     * @param array  $parameters Array of parameters to pass into method
      *
-     * @return mixed Method return.
+     * @return mixed Method return
      */
     private function invokeMethod(&$object, $methodName, array $parameters = [])
     {
--- Tests/Form/Type/OptionTypeTest.php
+++ Tests/Form/Type/OptionTypeTest.php
@@ -76,11 +76,11 @@ class OptionTypeTest extends TypeTestCase
     /**
      * Call protected/private method of a class.
      *
-     * @param object &$object    Instantiated object that we will run method on.
+     * @param object &$object    Instantiated object that we will run method on
      * @param string $methodName Method name to call
-     * @param array  $parameters Array of parameters to pass into method.
+     * @param array  $parameters Array of parameters to pass into method
      *
-     * @return mixed Method return.
+     * @return mixed Method return
      */
     private function invokeMethod(&$object, $methodName, array $parameters = [])
     {
