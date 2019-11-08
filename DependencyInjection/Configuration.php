d296cddb97a386ebe666b03a999508493e6a057b
Daniel Platt <dplatt@ukwebmedia.com>
2019-11-08 10:45:24 +0000

    Fix Deprecation warnings #31

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,9 +17,15 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('hackzilla_password_generator');
+        $treeBuilder = new TreeBuilder('hackzilla_password_generator');
 
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('hackzilla_password_generator');
+        }
+		
         return $treeBuilder;
     }
 }
